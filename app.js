c2bebe30978b3d926093c5cd2c5c095dded9962a
Hsac4lyfe <enzogianan@gmail.com>
2025-11-15 03:15:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -291,7 +291,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (DOM.cursor) {
           // Use CSS transform: translate3d for smoother, GPU-accelerated animation
           // e.clientX and e.clientY are relative to the viewport
-          DOM.cursor.style.transform =`translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+          DOM.cursor.style.transform =`translate3d(${e.clientX - 10 }px, ${e.clientY - 10}px, 0)`;
         }
         lastMove = now;
       }
@@ -321,5 +321,6 @@ document.addEventListener('DOMContentLoaded', () => {
   init();
 
 });
+
 
 
